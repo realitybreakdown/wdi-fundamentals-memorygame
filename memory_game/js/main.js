9be65b0400bfe3daebeedad0a226a9288b9086ae
Ryan <ryanarchambault1@gmailcom>
2018-08-29 21:49:53 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
 console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+ alert("You found a match!"); 
+ 	} else {
+ 		alert("Sorry, try again.");
+ 	}
