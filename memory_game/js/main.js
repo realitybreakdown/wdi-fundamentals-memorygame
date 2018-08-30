4019c0ba83e6fb0dd27f4e555122703035e28f3e
Ryan <ryanarchambault1@gmailcom>
2018-08-30 14:49:43 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,29 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen"
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped" + " " + cardOne);
-
-console.log("User flipped" + " " + cardTwo);
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
 
 var cardsInPlay = [];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -23,7 +34,10 @@ console.log("Sorry, try again.");
 }
 
 var flipCard = function(cardId) {
-console.log("User flipped " + cardOne);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank)
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 checkForMatch();
 };
 
